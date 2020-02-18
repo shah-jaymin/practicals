9b256a48d53d9d14da388a6ccb033965aa2c668c
Jaymin Shah <jaymin.shah@rapidops.com>
2020-02-18 12:11:29 +0530

    LMS

--- LMS/myIndexAdmin.js
+++ LMS/myIndexAdmin.js
@@ -1,17 +1,46 @@
 let str;
 let data;
 let userId;
-let users;
+let x= new Array();
+let len;
 let i;
-let len = localStorage.length;
+let temp;
 displayCourse();
 function displayCourse()
 {
     userId=sessionStorage.getItem("isLogin");
-    for ( var i = 0 ; i < len; ++i ) {
+    for ( var i = 0 ; i < localStorage.length; ++i ) {
         data=localStorage.getItem(localStorage.key(i));
         str=JSON.parse(data).isAdmin;
         if(!str)
-            document.getElementById('listing').innerHTML+=`<li class="list-group-item">${localStorage.key(i)}&ensp;<button class="btn btn-primary">HTML</button>&ensp;<button class="btn btn-primary">CSS</button>&ensp;<button class="btn btn-primary">Javascript</button></li>`;
+            document.getElementById('listing').innerHTML+=`<li class="list-group-item">${localStorage.key(i)}&ensp;<button value=${localStorage.key(i)} onclick="html(this.value)" class="btn btn-primary html">HTML</button>&ensp;<button value=${localStorage.key(i)} onclick="css(this.value)" class="btn btn-primary css">CSS</button>&ensp;<button value=${localStorage.key(i)} onclick="javascript(this.value)" class="btn btn-primary javascript">Javascript</button></li>`;
     }
 }
+function html(value){
+    str=localStorage.getItem(value);
+    x=JSON.parse(str).courses;
+    len=JSON.parse(str).courses.length;
+    x[len]='html';
+    temp = JSON.stringify( {password: JSON.parse(str).password, isAdmin: false, courses: x} )
+    localStorage.setItem(value, temp)
+}
+function css(value){
+    str=localStorage.getItem(value);
+    x=JSON.parse(str).courses;
+    len=JSON.parse(str).courses.length;
+    x[len]='css';
+    temp = JSON.stringify( {password: JSON.parse(str).password, isAdmin: false, courses: x} )
+    localStorage.setItem(value, temp)
+}
+function javascript(value){
+    str=localStorage.getItem(value);
+    x=JSON.parse(str).courses;
+    len=JSON.parse(str).courses.length;
+    x[len]='javascript';
+    temp = JSON.stringify( {password: JSON.parse(str).password, isAdmin: false, courses: x} )
+    localStorage.setItem(value, temp)
+}
+logout.addEventListener("click", function(){
+    window.location.replace("index.html");
+    sessionStorage.clear();
+});
--- LMS/myIndexStudent.js
+++ LMS/myIndexStudent.js
@@ -7,9 +7,13 @@ function displayCourse()
 {
     userId=sessionStorage.getItem("isLogin");
     str=localStorage.getItem(userId);
-    for(i=0; i<JSON.parse(str).course.length; i++)
+    for(i=0; i<JSON.parse(str).courses.length; i++)
     {
-        data=JSON.parse(str).course[i];
+        data=JSON.parse(str).courses[i];
         document.getElementById('listing').innerHTML+=`<li class="list-group-item">${data}</li>`;
     }
 }
+logout.addEventListener("click", function(){
+    window.location.replace("index.html");
+    sessionStorage.clear();
+});
